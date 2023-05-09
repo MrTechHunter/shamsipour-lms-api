a98014276d1f02d271521d9944ceecf794f9e8a5
mrtechhunter <mahdihamldar@gmail.com>
2023-05-09 12:24:53 +0430

    index.js updated

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require('dotenv').config()
 
-const app = express()
+// TODO:middleware
 
-app.get('/', (req, res) => {
-    res.send('Express JS on Vercel')
+app.use("/uploads", express.static("uploads"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+//TODO: Routes
+
+app.use("/auth", require("./routes/authRoute"));
+app.use("/", require("./routes/courseRoute"));
+app.use("/users", require("./routes/userRoute"));
+app.use("/profile", require("./routes/profileRoute"));
+app.use("/enroll-course", require("./routes/enrollRoute"));
+
+//TODO: Deploy:
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+//TODO: Database and server created
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({
+      message: "Hello"
+  });
 })
 
-app.get('/ping', (req, res) => {
-    res.send('pong 🏓')
-})
-
-const port = process.env.PORT || 8080
-
-app.listen(port, (err, res) => {
-    if (err) {
-        console.log(err)
-        return res.status(500).send(err.message)
-    } else {
-        console.log('[INFO] Server Running on port:', port)
-    }
-})
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected...");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Error occurred");
+  });
