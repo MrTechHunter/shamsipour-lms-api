7f6066806a9f484f6505102f951fd884151cd3ad
mrtechhunter <mahdihamldar@gmail.com>
2023-05-09 14:15:00 +0430

    index.js updated

--- index.js
+++ index.js
@@ -35,10 +35,8 @@ if (process.env.NODE_ENV == 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.json({
-      message: "Hello"
-  });
+app.get('/', (req, res) => {
+  res.send('Express JS on Vercel')
 })
 
 mongoose
